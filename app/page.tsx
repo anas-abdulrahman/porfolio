7bea2c817a9b0fc0098c5539873628df9fd4cbb4
anas-abulrahman <anaselmahdidx1999@gmail.com>
2025-10-05 12:03:28 +0200

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,17 @@
 
 
 import Approach from "@/components/Approach";
-import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import dynamic from "next/dynamic";
 import AboutMe from "../components/AboutMe";
+import Clients from "../components/Clients";
 import TechAndStack from "../components/TechAndStack";
-const Grid = dynamic(() => import("@/components/Grid"), {
-  ssr: false, // ✅ disables server-side rendering for this component
-});
+const Hero = dynamic(() => import("../components/Hero"));
+const Grid = dynamic(() => import("../components/Grid"));
+
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
